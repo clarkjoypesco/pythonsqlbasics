f88bec394e36ac7ac967c495ab7b2511c176799c
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-17 13:53:51 +0800

    insert query

--- sqlbasics.py
+++ sqlbasics.py
@@ -18,7 +18,7 @@ create table ordernames (
        t_order text,
        name text);
 
-       
+
 QUERY = '''
 select name, birthdate from animals where (not species = 'gorilla')
 and (not name= 'Max');
@@ -71,3 +71,40 @@ and birthdate <= '1998-12-31'
 # order by num desc
 # limit 5;
 # '''
+
+#
+# Write a query that returns all the species in the zoo, and how many animals of
+# each species there are, sorted with the most populous species at the top.
+# 
+# The result should have two columns:  species and number.
+#
+# The animals table has columns (name, species, birthdate) for each individual.
+# 
+QUERY = "select count(*) as num, species 
+        from animals
+        group by species
+        order by num desc"
+
+#
+# Insert a newborn baby opossum into the animals table and verify that it's
+# been added. To do this, fill in the rest of SELECT_QUERY and INSERT_QUERY.
+# 
+# SELECT_QUERY should find the names and birthdates of all opossums.
+# 
+# INSERT_QUERY should add a new opossum to the table, whose birthdate is today.
+# (Or you can choose any other date you like.)
+#
+# The animals table has columns (name, species, birthdate) for each individual.
+#
+
+SELECT_QUERY = '''
+select ... where ...;
+'''
+
+INSERT_QUERY = '''
+insert into animals values('Wibble','opussom','2019-4-17');
+'''
+
+INSERT_QUERY = '''
+insert into animals(name,species,birthdate) values('Wibble','opussom','2019-4-17');
+'''
